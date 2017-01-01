874f99b0419ceb014bf74d6e785ea5a8c8806ec8
gavin home window <619303764@qq.com>
2017-01-01 14:41:20 +0800

    web end chatroom

---
+++ Applications/Chat/Config/Db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/12/30 0030
+ * Time: 14:11
+ */
+
+class Db
+{
+    public static $db = [
+        'host'    => '127.0.0.1',
+        'port'    => 3306,
+        'user'    => 'root',
+        'password' => '123456',
+        'dbname'  => 'outside',
+        'charset'    => 'utf8mb4',
+    ];
+}
